4ec7464d9741074728e95b62b20bf37f2654a44f
Timo Ebel <timo.ebel@shopgate.com>
2025-03-28 15:30:25 +0100

    Updated release package script.

--- release/build_release_package.sh
+++ release/build_release_package.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-ZIP_FILE_NAME=shopgate-prestashop-integration.zip
+ZIP_FILE_NAME=shopgate-prestashop-integration-${TAG_NAME}.zip
 
-rm -rf src/vendors release/package $ZIP_FILE_NAME
+rm -rf vendor release/package $ZIP_FILE_NAME
+
 mkdir release/package && mkdir release/package/shopgate
 composer install -vvv --no-dev
 rsync -av --exclude-from './release/exclude-filelist.txt' ./src/ release/package/shopgate
@@ -12,5 +13,10 @@ rsync -av ./README.md release/package/shopgate
 rsync -av ./LICENSE.md release/package/shopgate
 rsync -av ./CONTRIBUTING.md release/package/shopgate
 rsync -av ./CHANGELOG.md release/package/shopgate
+
 cd release/package
-zip -r ../../$ZIP_FILE_NAME .
+zip -r ../$ZIP_FILE_NAME .
+cd ..
+rm -rf package
+cd ..
+rm -rf vendor
